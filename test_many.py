09429925d91b702e744de73b756ab8eb44d3d1de
fumingxing2015 <405118696@qq.com>
2022-05-03 16:26:38 +0800

    Add files via upload

--- test_many.py
+++ test_many.py
@@ -79,7 +79,7 @@ def get_balloon_dicts(img_dir):
     return dataset_dicts
 
 from detectron2.engine import DefaultTrainer
-def test_ballon():
+def train_ballon():
     for d in ["train", "val"]:
         DatasetCatalog.register("balloon_" + d, lambda d=d: get_balloon_dicts("/home/fu/data/balloon_dataset/balloon/" + d))
         MetadataCatalog.get("balloon_" + d).set(thing_classes=["balloon"])
@@ -99,8 +99,8 @@ def test_ballon():
     cfg.DATALOADER.NUM_WORKERS = 2
     cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
     cfg.SOLVER.IMS_PER_BATCH = 2
-    cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
-    cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
+    cfg.SOLVER.BASE_LR = 0.00001  # pick a good LR
+    cfg.SOLVER.MAX_ITER = 50000    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
     cfg.SOLVER.STEPS = []        # do not decay learning rate
     cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
@@ -108,10 +108,82 @@ def test_ballon():
 
     os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
     trainer = DefaultTrainer(cfg)
-    trainer.resume_or_load(resume=False)
+    trainer.resume_or_load(resume=True)
     trainer.train()
+
+from detectron2.utils.visualizer import ColorMode
+
+def test_ballon():
+
+    for d in ["train", "val"]:
+        DatasetCatalog.register("balloon_" + d, lambda d=d: get_balloon_dicts("/home/fu/data/balloon_dataset/balloon/" + d))
+        MetadataCatalog.get("balloon_" + d).set(thing_classes=["balloon"])
+    balloon_metadata = MetadataCatalog.get("balloon_val")
+
+    cfg=get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+    cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # path to the model we just trained
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set a custom testing threshold
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1
+    predictor = DefaultPredictor(cfg)
+    dataset_dicts = get_balloon_dicts("/home/fu/data/balloon_dataset/balloon/val")
+    for d in random.sample(dataset_dicts, 3):
+        print(d["file_name"])
+        im = cv2.imread(d["file_name"])
+        outputs = predictor(im)  # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
+        v = Visualizer(im[:, :, ::-1],
+                    metadata=balloon_metadata,
+                    scale=0.5,
+                    instance_mode=ColorMode.IMAGE_BW   # remove the colors of unsegmented pixels. This option is only available for segmentation models
+        )
+        if len(outputs["instances"])>0:
+            out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+            cv2.imshow("test",out.get_image()[:,:,::-1])
+            cv2.waitKey(0)
+        else:
+            print("no result")
+
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+from detectron2.data import build_detection_test_loader
+def evaluate_ballon():
+    for d in ["train", "val"]:
+        DatasetCatalog.register("balloon_" + d, lambda d=d: get_balloon_dicts("/home/fu/data/balloon_dataset/balloon/" + d))
+        MetadataCatalog.get("balloon_" + d).set(thing_classes=["balloon"])
+    balloon_metadata = MetadataCatalog.get("balloon_val")
+    cfg=get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+    cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # path to the model we just trained
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set a custom testing threshold
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1
+    predictor = DefaultPredictor(cfg)
+    evaluator = COCOEvaluator("balloon_val", output_dir="./output")
+    val_loader = build_detection_test_loader(cfg, "balloon_val")
+    print(inference_on_dataset(predictor.model, val_loader, evaluator))
+
+def test_keypoints():
+    cfg = get_cfg()   # get a fresh new config
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml"))
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7  # set threshold for this model
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml")
+    predictor = DefaultPredictor(cfg)
+    outputs = predictor(im)
+    v = Visualizer(im[:,:,::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    # cv2_imshow(out.get_image()[:, :, ::-1])
+
+def test_panoptic():
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-PanopticSegmentation/panoptic_fpn_R_101_3x.yaml"))
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-PanopticSegmentation/panoptic_fpn_R_101_3x.yaml")
+    predictor = DefaultPredictor(cfg)
+    panoptic_seg, segments_info = predictor(im)["panoptic_seg"]
+    v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+    out = v.draw_panoptic_seg_predictions(panoptic_seg.to("cpu"), segments_info)
+    # cv2_imshow(out.get_image()[:, :, ::-1])
 
 if __name__ == '__main__':
 
     #small_test()
-    test_ballon()
+    # train_ballon()
+    # test_ballon()
+    evaluate_ballon()
